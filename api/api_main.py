9f592daa850efe63f371656f131054b65ed5097f
PhoenixVertexPro <nexa.veper@gmail.com>
2026-08-01 13:04:18 +0330

    Update api_main.py

--- api/api_main.py
+++ api/api_main.py
@@ -1,1 +1,14 @@
 
+from flask import Blueprint, jsonify
+from engine.core import engine_status
+from engine.signals import generate_signal
+
+api = Blueprint("api", __name__)
+
+@api.route("/engine")
+def engine_info():
+    return jsonify(engine_status())
+
+@api.route("/signal")
+def signal_info():
+    return jsonify(generate_signal())
