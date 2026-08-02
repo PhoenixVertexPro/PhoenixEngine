a431c45ed639c76331ea6cd9fc469ebb83be215b
PhoenixVertexPro <nexa.veper@gmail.com>
2026-08-02 19:20:28 +0330

    Update referral_engine.py

--- referral/referral_engine.py
+++ referral/referral_engine.py
@@ -1,1 +1,15 @@
+# referral/referral_engine.py
 
+def get_referral_links():
+    return [
+        {
+            "exchange": "BingX",
+            "profit": "50%",
+            "link": "https://bingx.com/ref/phoenixengine"
+        },
+        {
+            "exchange": "Bybit",
+            "profit": "40%",
+            "link": "https://bybit.com/ref/phoenixengine"
+        }
+    ]
