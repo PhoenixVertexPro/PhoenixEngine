6f42b802aa6a947628e0c18efaa18386d13160b3
PhoenixVertexPro <nexa.veper@gmail.com>
2026-08-02 20:45:07 +0330

    Update main.py
    # start_api()
    # start_dashboard()
    # start_scheduler()

--- main.py
+++ main.py
@@ -1,7 +1,1 @@
-# force update
-from bot.bot_main import start_bot
-from dashboard.dashboard_main import start_dashboard
-
-if __name__ == "__main__":
-    start_bot()
-    start_dashboard()
+start_bot()
