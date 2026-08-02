c0130514bb1cee7720dc5b2300d4f5086d567c36
PhoenixVertexPro <nexa.veper@gmail.com>
2026-08-02 19:48:01 +0330

    Create analytics.py

---
+++ reports/analytics.py
@@ -0,0 +1,10 @@
+# reports/analytics.py
+
+def generate_report():
+    return {
+        "vip_count": 42,
+        "monthly_income": "12,500,000 تومان",
+        "referral_income": "4,200,000 تومان",
+        "growth_rate": "18%",
+        "active_users": 310
+    }
